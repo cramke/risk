9fae1af1211d66d69075a97f14f0882957d27ef9
Carsten Ramke <carsten.ramke@gmail.com>
2022-06-11 18:15:08 +0200

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include "main.h"
 
 int main() {
+    pqxx::connection c("dbname=some_postgis user=postgres password=postgis port=5432");
+    std::cout << "Something worked.";
+    if (c.is_open())
+        std::cout << "Opened database successfully: " << c.dbname() << std::endl;
+    else
+        std::cout << "Can't open database" << std::endl;
+
     std::cout << "Hello Risk!" << std::endl;
 }
--- src/main.h
+++ src/main.h
@@ -1,4 +1,19 @@
+/**
+ * @file main.h
+ * @author Carsten Ramke (carsten.ramke@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+# pragma once
 #include <iostream>
+#include <stdlib.h>
+
+#include <pqxx/pqxx>
+
 #include <ompl/control/SpaceInformation.h>
 #include <ompl/base/goals/GoalState.h>
 #include <ompl/base/spaces/SE2StateSpace.h>
@@ -7,6 +22,5 @@
 #include <ompl/control/planners/rrt/RRT.h>
 #include <ompl/control/SimpleSetup.h>
 #include <ompl/config.h>
-#include <iostream>
 #include <valarray>
 #include <limits>
